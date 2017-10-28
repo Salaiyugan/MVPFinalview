5e1c5c121463d7bddd5c641b27b7dee89c806f0a
Salaiyugan <32095207+Salaiyugan@users.noreply.github.com>
2017-10-28 19:23:06 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,50 @@
 angular.module('myapp', ["ui.router"])
     .config(function($stateProvider, $urlRouterProvider){
 
-      $urlRouterProvider.otherwise("/home");
+			$stateProvider
+			.state('root', {
+				abstract: true,
+				url:''
+			})
+      .state('root.home', {
+				url: '/home',
+				templateUrl: 'view/home.html',
+        controller: 'homeCtrl',
+			})
 
-      $stateProvider
-          .state('root', {
-              abstract: true,
-              url:''
-          })
-          .state('root.home', {
-              url: '/home',
-              templateUrl: 'views/home.html',
-              onEnter: function(){
-                  console.log("enter contacts.detail");
-              }
-          }).state('root.home.company', {
-              url: '/company',
-              templateUrl: 'views/company/companyPanel.html',
-              //controller:'CompanyCtrl'
-              onEnter: function(){
-                  console.log("enter contacts.detail");
-              }
-          }).state('root.home.employee', {
-              url: '/employee',
-              templateUrl: 'views/employee/employeePanel.html',
-              onEnter: function(){
-                  console.log("enter contacts.detail");
-              }
-          });
+      .state('root.login', {
+				url: '/login',
+				templateUrl: 'view/login.html',
+        controller: 'loginCtrl',
+			})
+      .state('root.signup', {
+				url: '/signup',
+				templateUrl: 'view/signup.html',
+        controller:'loginCtrl',
+			})
+      .state('root.logout', {
+				url: '/logout',
+				templateUrl: 'view/logout.html',
+			})
+
+      .state('root.home.profile',{
+        url:'/profile',
+        templateUrl:'view/profile.html',
+        controller:'loginCtrl',
+      })
+
+      .state('root.home.mycart',{
+        url:'/mycart',
+        templateUrl:'view/mycart.html',
+        controller:'homeCtrl',
+      })
+
+
+      .state('root.home.product',{
+        url:'/product',
+        templateUrl:'view/product.html',
+        controller:'productCtrl',
+      })
+
+			$urlRouterProvider.otherwise("/login");
     });
